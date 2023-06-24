0ce831fcd1851f3182e110da9ac61faae7d89f2a
NNahkan <hoksuz2424@gmail.com>
2023-06-24 08:52:22 -0400

    Learning how ts works slowly

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 
 
+import { useState } from 'react'
 import './App.css'
 import DummyComponent from './DummyProps'
 
 
 
 function App() {
-	const setNumber = () => console.log("works")
+	const [number, setNumber] = useState(10)
 
 
 	return (
-		<DummyComponent number={10} setNumber={setNumber} />
+		<DummyComponent number={number} setNumber={setNumber} />
 	)
 }
 
