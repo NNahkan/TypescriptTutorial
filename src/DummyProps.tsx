5b97938a31b63f4b513ee5d52c1249ee3536ce31
NNahkan <hoksuz2424@gmail.com>
2023-07-08 19:16:18 -0400

    Trying types

--- src/DummyProps.tsx
+++ src/DummyProps.tsx
@@ -24,6 +24,18 @@ const cocktails: Cocktail[] = [
 	},
 ];
 
+type UserInputSanitizedString = string;
+ 
+function sanitizeInput(str: string): UserInputSanitizedString {
+  return sanitize(str);
+}
+ 
+// Create a sanitized input
+let userInput = sanitizeInput(getInput());
+ 
+// Can still be re-assigned with a string though
+userInput = "new input";
+
 const DummyComponent = () => {
 	/* 
 		const [cocktails, setCocktails] = useState<Cocktail[]>([]);
