bca1dab665246d2a72d8c0fb0094a931f38d0ef6
NNahkan <hoksuz2424@gmail.com>
2023-07-09 19:53:12 -0400

    Starting the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 
 
-import { useState } from 'react'
 import './App.css'
-import DummyComponent from './DummyProps'
 
 
-
-function App() {
-	const [number, setNumber] = useState(10)
+const App:React.FC = () => {
 
 
 	return (
-		<DummyComponent number={number} setNumber={setNumber} />
+		<div className='App' >
+			<span className='heading'>Taskify</span>
+		</div>
 	)
 }
 
 export default App
+
+/* 	let hobbies: string[];
+	let role: [number, string];
+	
+	type Person = {
+		name: string;
+		age?: number;
+	}
+	
+	let person: Person = {
+		name: "ali",
+		age: 22,
+	}
+	
+	let lostOfPeople: Person[]; 
+
+let printName: (name: string) => void | never;
+let personName: unknown;
+*/
