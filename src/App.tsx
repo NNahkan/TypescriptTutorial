e8cff1cfafd0864e2f6410aa08c7099187c6f82c
NNahkan <hoksuz2424@gmail.com>
2023-07-11 12:56:06 -0400

    Trying types

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@
 import './App.css'
 
 
-const App:React.FC = () => {
+const App: React.FC = () => {
+	let role: [number, string];
 
-
+	type Person = {
+		name: string;
+		age?: number;
+	}
 	return (
 		<div className='App' >
 			<span className='heading'>Taskify</span>
@@ -15,12 +19,12 @@ const App:React.FC = () => {
 
 export default App
 
-/* 	let hobbies: string[];
-	let role: [number, string];
+/* 	t role: [number, string];
 	
 	type Person = {
 		name: string;
-		age?: number;
+		age?: number;let hobbies: string[];
+	le
 	}
 	
 	let person: Person = {
