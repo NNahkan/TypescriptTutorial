ef6a60d0d41bbff5737ffa131d1a9acc7a5816cb
NNahkan <hoksuz2424@gmail.com>
2023-07-13 22:08:48 -0400

    Using useRef for enter button

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,11 +4,12 @@ import "./styles.css"
 interface Props {
 	todo: string;
 	setTodo: React.Dispatch<React.SetStateAction<string>>;
+	handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField = ({ todo, setTodo }: Props) => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
 	return (
-		<form className='input' action="">
+		<form className='input' onSubmit={handleAdd}>
 			{/* bem convention */}
 			<input value={todo} onChange={(e) => setTodo(e.target.value)} placeholder='Enter a Task' className='input__box' />
 			<button className='input_submit'>Go</button>
